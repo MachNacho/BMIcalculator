e9903b957601bd765bf3ccc7666d8817f47b73dd
MachNacho <64479526+MachNacho@users.noreply.github.com>
2023-08-07 21:12:37 +0200

    edits

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,41 +29,128 @@
         private void InitializeComponent()
         {
             RBMetric = new RadioButton();
-            radioButton2 = new RadioButton();
+            RBImperial = new RadioButton();
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            RollHeight = new NumericUpDown();
+            RollWeight = new NumericUpDown();
+            lblBMIvalue = new Label();
+            BtnCalculate = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)RollHeight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)RollWeight).BeginInit();
             SuspendLayout();
             // 
             // RBMetric
             // 
             RBMetric.AutoSize = true;
-            RBMetric.Location = new Point(12, 12);
+            RBMetric.Checked = true;
+            RBMetric.Location = new Point(3, 9);
             RBMetric.Name = "RBMetric";
-            RBMetric.Size = new Size(94, 19);
+            RBMetric.Size = new Size(59, 19);
             RBMetric.TabIndex = 0;
             RBMetric.TabStop = true;
-            RBMetric.Text = "radioButton1";
+            RBMetric.Text = "Metric";
             RBMetric.UseVisualStyleBackColor = true;
             RBMetric.CheckedChanged += radioButton1_CheckedChanged;
             // 
-            // radioButton2
+            // RBImperial
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(12, 37);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(94, 19);
-            radioButton2.TabIndex = 1;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "radioButton2";
-            radioButton2.UseVisualStyleBackColor = true;
+            RBImperial.AutoSize = true;
+            RBImperial.Location = new Point(129, 9);
+            RBImperial.Name = "RBImperial";
+            RBImperial.Size = new Size(68, 19);
+            RBImperial.TabIndex = 1;
+            RBImperial.Text = "Imperial";
+            RBImperial.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(RBMetric);
+            panel1.Controls.Add(RBImperial);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(202, 41);
+            panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(146, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Height";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(146, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Weight";
+            // 
+            // RollHeight
+            // 
+            RollHeight.DecimalPlaces = 1;
+            RollHeight.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            RollHeight.Location = new Point(12, 93);
+            RollHeight.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+            RollHeight.Name = "RollHeight";
+            RollHeight.Size = new Size(120, 23);
+            RollHeight.TabIndex = 9;
+            // 
+            // RollWeight
+            // 
+            RollWeight.DecimalPlaces = 1;
+            RollWeight.Increment = new decimal(new int[] { 1, 0, 0, 65536 });
+            RollWeight.Location = new Point(12, 134);
+            RollWeight.Maximum = new decimal(new int[] { 9999, 0, 0, 0 });
+            RollWeight.Name = "RollWeight";
+            RollWeight.Size = new Size(120, 23);
+            RollWeight.TabIndex = 10;
+            // 
+            // lblBMIvalue
+            // 
+            lblBMIvalue.AutoSize = true;
+            lblBMIvalue.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            lblBMIvalue.Location = new Point(42, 291);
+            lblBMIvalue.Name = "lblBMIvalue";
+            lblBMIvalue.Size = new Size(90, 37);
+            lblBMIvalue.TabIndex = 11;
+            lblBMIvalue.Text = "label3";
+            lblBMIvalue.Click += lblBMIvalue_Click;
+            // 
+            // BtnCalculate
+            // 
+            BtnCalculate.Location = new Point(15, 202);
+            BtnCalculate.Name = "BtnCalculate";
+            BtnCalculate.Size = new Size(75, 23);
+            BtnCalculate.TabIndex = 12;
+            BtnCalculate.Text = "Calculate";
+            BtnCalculate.UseVisualStyleBackColor = true;
+            BtnCalculate.Click += BtnCalculate_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(358, 511);
-            Controls.Add(radioButton2);
-            Controls.Add(RBMetric);
+            Controls.Add(BtnCalculate);
+            Controls.Add(lblBMIvalue);
+            Controls.Add(RollWeight);
+            Controls.Add(RollHeight);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
             Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)RollHeight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RollWeight).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -71,6 +158,13 @@
         #endregion
 
         private RadioButton RBMetric;
-        private RadioButton radioButton2;
+        private RadioButton RBImperial;
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown RollHeight;
+        private NumericUpDown RollWeight;
+        private Label lblBMIvalue;
+        private Button BtnCalculate;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,5 +11,40 @@ namespace BMIcalculator
         {
 
         }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            float Weight = (float)RollWeight.Value;
+            float Height = (float)RollHeight.Value;
+
+            if (RBImperial.Checked == true)
+            {
+                BmiCalc(Weight, Height,'I');
+                
+            }
+            else if (RBMetric.Checked == true)
+            {
+                BmiCalc(Weight, Height, 'M');
+            }
+           
+        }
+
+        private void lblBMIvalue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private double BmiCalc(float Weight,float Height, char Metric) 
+        {
+            if(Metric == 'I')
+            {
+                return (703 * Weight / (Math.Pow(Height, 2)));
+            }
+            else 
+            {
+                return (Weight / (Math.Pow(Height, 2)));
+            }
+           
+        }
     }
 }
